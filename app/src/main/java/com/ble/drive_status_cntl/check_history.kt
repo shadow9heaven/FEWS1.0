c90c242f85d7c8bc42809418efdf75a4b20a601e
Roxus <roxusdai@nitring.com>
2021-05-20 15:38:00 +0800

    [0520] first version upload

---
+++ app/src/main/java/com/ble/drive_status_cntl/check_history.kt
@@ -0,0 +1,11 @@
+package com.ble.drive_status_cntl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class check_history : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_history)
+    }
+}
---
+++ app/src/main/java/com/ble/drive_status_cntl/wave_adjust.kt
@@ -0,0 +1,11 @@
+package com.ble.drive_status_cntl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class wave_adjust : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wave_adjust)
+    }
+}
